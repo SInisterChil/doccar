4e67f3e5da2b48efeceda3fd3957f96ebd949bc8
sumitthrita <sumitthrita@gmail.com>
2022-07-04 23:17:22 +0530

    Deploy doccar v0.1.0

---
+++ precache-manifest.ce0f4952dcb801e376e87acd649b7ebd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c16b5f309e2471b144ad20975f10e08",
+    "url": "/doccar/index.html"
+  },
+  {
+    "revision": "952b0b4de69a2890140d",
+    "url": "/doccar/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "bd443201f88d0027a10f",
+    "url": "/doccar/static/css/main.e18abca6.chunk.css"
+  },
+  {
+    "revision": "952b0b4de69a2890140d",
+    "url": "/doccar/static/js/2.c62fdd5c.chunk.js"
+  },
+  {
+    "revision": "b6c269b6374f988b4fe55daf3bdb7082",
+    "url": "/doccar/static/js/2.c62fdd5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ee9cb98b8713e3d2ee",
+    "url": "/doccar/static/js/3.cfa287f7.chunk.js"
+  },
+  {
+    "revision": "bd443201f88d0027a10f",
+    "url": "/doccar/static/js/main.3d7a4a5a.chunk.js"
+  },
+  {
+    "revision": "c679d91ff7905391a508",
+    "url": "/doccar/static/js/runtime-main.e422fd4f.js"
+  },
+  {
+    "revision": "f63b921d8053c889bebe99a4102d6017",
+    "url": "/doccar/static/media/drumBrakeTuning.f63b921d.png"
+  },
+  {
+    "revision": "80e2e6f1582ea7fb883d6b75d0f19a43",
+    "url": "/doccar/static/media/frontBrakeDisk.80e2e6f1.png"
+  },
+  {
+    "revision": "9bf7bf1ffe2798bf269066be4ad0ffc6",
+    "url": "/doccar/static/media/radiatorFlushClean.9bf7bf1f.png"
+  },
+  {
+    "revision": "ba6cbcd715a40113dab162d490dbd946",
+    "url": "/doccar/static/media/radiatorReplace.ba6cbcd7.png"
+  },
+  {
+    "revision": "5e8923b651102e93535b3f274e0c28b0",
+    "url": "/doccar/static/media/sPBasic.5e8923b6.png"
+  },
+  {
+    "revision": "ba6cbcd715a40113dab162d490dbd946",
+    "url": "/doccar/static/media/sPComprehensive.ba6cbcd7.png"
+  },
+  {
+    "revision": "4b13d93a0478d6e38611bbda413823bf",
+    "url": "/doccar/static/media/sPStandard.4b13d93a.png"
+  }
+]);
